d81341686c9307ddc000e7b2504f0b500c58bb61
Nicolas Machado <nicoma63@gmail.com>
2017-02-23 14:39:26 +0100

    Added more questions

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ var controller = {
     questions: [
         {
             type: "genre",
-            question: "Looks like your plane is going down. Your parachute might or might not work...",
+            question: "Your plane is acting weird. Your parachute might or might not work...",
             answer1: "JUMP!",
             answer2: "Who's in charge here? Land that plane NOW!",
             effect: [[12, 1], [28, 3], [27, 1], [10749, -1]]
@@ -38,7 +38,7 @@ var controller = {
         {
             type: "genre",
             question: "A man and a woman bump into each other...",
-            answer1: "They'll fall in love for sure",
+            answer1: "What a beautiful story",
             answer2: "They settle this in a fist fight",
             effect: [[28, -1], [80, -2], [27, -1], [10749, 2]]
         },
@@ -72,16 +72,16 @@ var controller = {
         },
         {
             type: "genre",
-            question: "Jane from accounting said you were kind of a drama-queen",
+            question: "Jane from accounting said you were too dramatic",
             answer1: "Whatever floats her boat",
-            answer2: "That is NOT TRUE!",
+            answer2: "That is NOT TRUE! *Storms out*",
             effect: [[18, -3], [14, -1]]
         },
         {
             type: "genre",
-            question: "Children",
-            answer1: "Yay!",
-            answer2: "Eww!",
+            question: "Children are",
+            answer1: "Funny",
+            answer2: "Boring",
             effect: [[16, 1], [35, 2], [80, -1], [10751, 3], [27, -1]]
         },
         {
@@ -101,8 +101,8 @@ var controller = {
         {
             type: "genre",
             question: "What would you rather lose?",
-            answer1: "Your speech",
-            answer2: "Your ears",
+            answer1: "Your sight",
+            answer2: "Your audition",
             effect: [[28, 1], [18, -1], [10402, 2]]
         },
         {
@@ -137,8 +137,218 @@ var controller = {
             type: "genre",
             question: "Hierarchy is",
             answer1: "Essential",
-            answer2: "Stupid",
+            answer2: "Pointless",
             effect: [[28, 1], [80, 1], [27, 1], [10749, -1], [53, 1]]
+        },
+        {
+            type: "genre",
+            question: "You have the technology to assemble one ultra high-tech robot",
+            answer1: "Make it strong",
+            answer2: "Make it smart",
+            effect: [[28, 2], [12, 1], [99, -1], [10751, -1], [10749, -1]]
+        },
+        {
+            type: "genre",
+            question: "You get one free back and forth trip to the moon",
+            answer1: "Err... no thanks",
+            answer2: "To the moon!",
+            effect: [[28, -1], [12, -2], [9648, -1], [878, -1]]
+        },
+        {
+            type: "genre",
+            question: "How good are you at drawing?",
+            answer1: "Decent",
+            answer2: "Not good",
+            effect: [[16, 1], [10751, 1], [878, 1]]
+        },
+        {
+            type: "genre",
+            question: "Communism jokes are not funny",
+            answer2: "Unless everyone gets them",
+            answer1: "They're not indeed",
+            effect: [[28, 1], [12, 1], [35, -2]]
+        },
+        {
+            type: "genre",
+            question: "Would you rather work at",
+            answer1: "The FBI",
+            answer2: "A theme park",
+            effect: [[28, 1], [16, -1], [80, 2], [14, -1]]
+        },
+        {
+            type: "genre",
+            question: "What is better",
+            answer1: "An inconvenient truth",
+            answer2: "An comfortable lie",
+            effect: [[12, 1], [99, 1], [18, 1], [14, -1], [9648, -1], [878, -1]]
+        },
+        {
+            type: "genre",
+            question: "Dogs or cats?",
+            answer1: "Dogs",
+            answer2: "Cats",
+            effect: [[18, 1], [10751, 1], [27, -1], [9648, -1], [10749, 1]]
+        },
+        {
+            type: "genre",
+            question: "Wanna go out tonight?",
+            answer1: "Nope",
+            answer2: "Sure",
+            effect: [[28, -1], [12, -1], [10751, 1], [14, 1], [10749, 1]]
+        },
+        {
+            type: "genre",
+            question: "What is a troll?",
+            answer1: "An imaginary creature",
+            answer2: "A person who sows discord on the Internet ",
+            effect: [[14, 2], [9648, 1]]
+        },
+        {
+            type: "genre",
+            question: "Would you rather be a Master of",
+            answer1: "Evil",
+            answer2: "Good",
+            effect: [[80, 1], [27, 2], [9648, 1]]
+        },
+        {
+            type: "genre",
+            question: "What is the best support for music?",
+            answer1: "Physical",
+            answer2: "Digital",
+            effect: [[99, 1], [10402, 1]]
+        },
+        {
+            type: "genre",
+            question: "Have aliens ever visited Earth?",
+            answer1: "Probably",
+            answer2: "Probbaly not",
+            effect: [[14, 1], [9648, 2], [878, 1]]
+        },
+        {
+            type: "genre",
+            question: "What is love?",
+            answer1: "Baby don't hurt me",
+            answer2: "A profoundly tender, passionate affection for another person.",
+            effect: [[35, 1], [18, -1], [10749, -2]]
+        },
+        {
+            type: "genre",
+            question: "Do you think humans will eventually settle on other planets?",
+            answer1: "Definitely",
+            answer2: "No way",
+            effect: [[12, 1], [14, 1], [27, 1], [878, 2]]
+        },
+        {
+            type: "genre",
+            question: "The human race is",
+            answer1: "Too smart",
+            answer2: "Too simple",
+            effect: [[80, 1], [18, 1], [27, 1], [53, 1]]
+        },
+        {
+            type: "genre",
+            question: "How do you answer someone who tells you you can't spell?",
+            answer1: "Gentleman, your mistaken",
+            answer2: "You're wrong",
+            effect: [[28, 1], [35, 1], [80, 1], [53, 1]]
+        },
+        {
+            type: "genre",
+            question: "Look, it's raining outside",
+            answer1: "When is the sun coming back?",
+            answer2: "Whatever",
+            effect: [[28, -1], [12, -2], [878, -1]]
+        },
+        {
+            type: "genre",
+            question: "How often do you clean up after yourself?",
+            answer1: "Rarely",
+            answer2: "Often",
+            effect: [[16, 1], [10751, 1], [14, 1], [10749, 1]]
+        },
+        {
+            type: "genre",
+            question: "How would you defuse a tense situation?",
+            answer1: "With patience",
+            answer2: "With a joke",
+            effect: [[28, 1], [35, -1], [10751, 1], [10749, 1]]
+        },
+        {
+            type: "genre",
+            question: "Are you good at remembering names?",
+            answer1: "Very",
+            answer2: "Who are you, already?",
+            effect: [[80, 2], [18, 1], [9648, 1], [53, 1]]
+        },
+        {
+            type: "genre",
+            question: "What's the most important?",
+            answer1: "Knowledge",
+            answer2: "Intelligence",
+            effect: [[99, 1], [9648, 1], [878, 1], [53, 1]]
+        },
+        {
+            type: "genre",
+            question: "Are you sure you would like to be able to read minds?",
+            answer1: "Hell yeah",
+            answer2: "That might not be a good thing",
+            effect: [[12, 1], [18, 1], [14, 1], [878, 1]]
+        },
+        {
+            type: "genre",
+            question: "What do you currently have?",
+            answer1: "A stable life",
+            answer2: "An adventurous life",
+            effect: [[28, -1], [12, -1], [10751, 1], [14, -1], [27, 1], [9648, -1]]
+        },
+        {
+            type: "genre",
+            question: "Who's the strongest?",
+            answer1: "Gandalf",
+            answer2: "Superman",
+            effect: [[28, -1], [12, -1], [14, 2], [878, -1]]
+        },
+        {
+            type: "genre",
+            question: "Ghosts are",
+            answer1: "Spooky",
+            answer2: "Silly",
+            effect: [[28, 1], [27, 2], [9648, 1], [878, 1]]
+        },
+        {
+            type: "genre",
+            question: "What field would you rather have expertise in?",
+            answer1: "Playing music",
+            answer2: "Playing sports",
+            effect: [[28, -1], [10402, 1]]
+        },
+        {
+            type: "genre",
+            question: "Are you afraid of the dark?",
+            answer1: "No",
+            answer2: "That's not funny, turn the light back on!",
+            effect: [[80, -1], [14, 1], [27, 1], [9648, 2]]
+        },
+        {
+            type: "genre",
+            question: "Do you remember the name of your first crush?",
+            answer1: "Yes",
+            answer2: "Nope",
+            effect: [[35, 1], [18, 1], [10751, 1], [10749, 1]]
+        },
+        {
+            type: "genre",
+            question: "Science",
+            answer1: "Is awesome",
+            answer2: "Has gone too far",
+            effect: [[28, -1], [12, 1], [14, 1], [878, 2]]
+        },
+        {
+            type: "genre",
+            question: "Do you answer anonymous calls at night?",
+            answer1: "Never",
+            answer2: "Sure",
+            effect: [[14, 1], [27, 1], [9648, 1], [53, 1]]
         }
     ]
 };
